673db9f1eb4c102daa882c7efe86ff38723c5590
tsuruno <tsuruno@seamaple.co.jp>
2022-07-01 16:51:14 +0900

    first commit

---
+++ src/main/java/com/example/hello_iijimasann/HelloIijimasannApplication.java
@@ -0,0 +1,13 @@
+package com.example.hello_iijimasann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloIijimasannApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloIijimasannApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hello_iijimasann/HelloIijimasannApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello_iijimasann;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloIijimasannApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
